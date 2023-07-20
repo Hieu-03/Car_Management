4a0e5c41a85c26f598a4796eb9436f1ef1fdc58c
Hieu-03 <hieupnse171321@fpt.edu.vn>
2023-07-20 22:08:36 +0700

    first commit

---
+++ Lab_01/AutomobileWinApp/frmCarDetails.cs
@@ -0,0 +1,63 @@
+﻿using AutomobileLibrary.BusinessObject;
+using AutomobileLibrary.Repository;
+
+namespace AutomobileWinApp
+{
+    public partial class frmCarDetails : Form
+    {
+        public frmCarDetails()
+        {
+            InitializeComponent();
+        }
+
+        public ICarRepository CarRepository { get; set; }
+        public bool InsertOrUpdate { get; set; }
+        public Car CarInfo { get; set; }
+
+        private void frmCarDetails_Load(object sender, EventArgs e)
+        {
+            cboManufacturer.SelectedIndex = 0;
+            txtCarID.Enabled = !InsertOrUpdate;
+            if (InsertOrUpdate == true)
+            {
+                txtCarID.Text = CarInfo.CarID.ToString();
+                txtCarName.Text = CarInfo.CarName;
+                txtPrice.Text = CarInfo.Price.ToString();
+                txtReleaseYear.Text = CarInfo.ReleaseYear.ToString();
+                cboManufacturer.Text = CarInfo.Manufacturer.Trim();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var car = new Car
+                {
+                    CarID = int.Parse(txtCarID.Text),
+                    CarName = txtCarName.Text,
+                    Manufacturer = cboManufacturer.Text,
+                    Price = decimal.Parse(txtPrice.Text),
+                    ReleaseYear = int.Parse(txtReleaseYear.Text)
+                };
+                if (InsertOrUpdate == false)
+                {
+                    //CarRepository carRepository = new CarRepository(); carRepository.InsertCar(car);
+                    CarRepository.InsertCar(car);
+                    MessageBox.Show("Add success!!");
+                }
+                else
+                {
+                    //CarRepository carRepository = new CarRepository(); carRepository.UpdateCar(car);
+                    CarRepository.UpdateCar(car);
+                    MessageBox.Show("Update success!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, InsertOrUpdate == false ? "Add a new car" : "Update a car");
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e) => Close();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Car_Management
-Lab_01 AutomobileManagement Using LINQ and WinForms and Lab_02 AutomobileManagement Using ADO.NET and WinfForms
+This is a personal project include 2 labs using C#, WinForms, SQL server, etc
+- Lab_01 AutomobileManagement Using LINQ and WinForms
+- Lab_02 AutomobileManagement Using ADO.NET and WinfForms
+
+This project aim to create forms to manage cars (Add, Update, Delete, View)
+
+Here are some screenshots of the forms:
+
+![The form after the database is loaded.](/Car_management_screenshots/Load_form.jpeg "Load form")
+
+This is the form after clicking "Load" button.
+
+![The form to update car details.](/Car_management_screenshots/Update.jpeg "Update")
+
+This is loaded by double clicking on the car row which you want to update.
+
+![The form to add a new car.](/Car_management_screenshots/Add.jpeg "Add")
+
+This is loaded by clicking on the "Add" button.
